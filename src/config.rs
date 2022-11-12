c2fe11280390105eb5159da7938f8bf75296504c
TundraClimate <tundra.tundere@gmail.com>
2022-11-13 00:06:55 +0900

    feat: config path_checker

--- src/config.rs
+++ src/config.rs
@@ -3,10 +3,22 @@ use std::path::PathBuf;
 
 pub struct Config {
     pub from_path: PathBuf,
+    pub out_path: PathBuf,
 }
 
-pub fn load_config(args: &Args) -> Config {
+pub fn load_config(args: &Args) -> Result<Config, String> {
     let args: Vec<_> = args.collect();
-    let from_path = PathBuf::from(String::from(&args[1]));
-    Config { from_path }
+    if check_from_path(&args) {} else { return Err(String::from("Not Specified *.mhl path.")); }
+    let from_path = PathBuf::from(&args[1]);
+    let mut out_path = PathBuf::from(String::from("."));
+    if check_out_path(&args) { out_path = PathBuf::from(&args[2]) }
+    Ok(Config { from_path, out_path })
+}
+
+fn check_from_path(args: &Vec<String>) -> bool {
+    args.len() >= 2
+}
+
+fn check_out_path(args: &Vec<String>) -> bool {
+    args.len() >= 3
 }
